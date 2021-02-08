6367b7daf2fe594f48645d087830fd98bbc9b187
AhmedShibbir <shibbirahmedops@gmail.com>
2021-02-09 00:45:03 +0600

    C# project

---
+++ Registration.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SellEx
+{
+    public partial class Registration : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+            con.Open();
+            if (TextBox3.Text == TextBox4.Text)
+            {
+                SqlCommand cmd = new SqlCommand("insert into Users(Name,Email,Password)values(@Name, @Email, @Password)", con);
+                cmd.Parameters.AddWithValue("@Name", TextBox1.Text);
+                cmd.Parameters.AddWithValue("@Email", TextBox2.Text);
+                cmd.Parameters.AddWithValue("@Password", TextBox3.Text);
+                cmd.ExecuteNonQuery();
+            }
+            else
+            {
+                Label1.Visible = true;
+            }
+        }
+    }
+}
